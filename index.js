b283041fd7e47879bd411c1039c91864cb3ac514
Pratham <ppmk632@gmail.com>
2022-03-02 23:29:07 +0530

    C

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
